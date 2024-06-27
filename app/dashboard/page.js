d90e659927f24f9367b89bc3667d632fbb80bd6e
shohidul2015 <shd_islam@hotmail.com>
2024-06-27 13:30:15 +0200

    first commit

---
+++ app/dashboard/page.js
@@ -0,0 +1,11 @@
+
+
+const page = () => {
+  return (
+    <div>
+      <h1>Dashboard!</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ app/greeting/[name]/page.js
@@ -0,0 +1,11 @@
+
+
+const page = ({params}) => {
+  return (
+    <div>
+      <h1>Welcom to the journey, {params.name}</h1>
+    </div>
+  )
+}
+
+export default page
